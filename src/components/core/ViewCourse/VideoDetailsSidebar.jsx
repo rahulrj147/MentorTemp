fbf55aa330b73058d1d56cf93d7e73740eab996c
rahulrj147 <rahuljha1771@gmail.com>
2025-02-17 18:46:16 +0530

    Your commit message

--- src/components/core/ViewCourse/VideoDetailsSidebar.jsx
+++ src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -40,7 +40,7 @@ export default function VideoDetailsSidebar({ setReviewModal }) {
 
   return (
     <>
-      <div className="flex mt-24 h-[calc(100vh-3.5rem)] w-[320px] max-w-[350px] flex-col border-r-[1px] border-r-gray-700 bg-gray-800">
+      <div className="flex mt-20 h-[calc(100vh-3.5rem)] w-[320px] max-w-[350px] flex-col border-r-[1px] border-r-gray-700 bg-gray-800">
         <div className="mx-5 flex flex-col items-start justify-between gap-2 gap-y-4 border-b border-gray-600 py-5 text-lg font-bold text-gray-50">
           <div className="flex w-full items-center justify-between ">
             <div
