dda9883521ce3476636116f3b9bf5cd86370eaed
rahulrj147 <rahuljha1771@gmail.com>
2025-02-17 15:43:31 +0530

    check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,10 +19,10 @@ function Home() {
   return (
     <div>
       {/* Section 1 */}
-      <div className="relative mx-auto my-10 flex w-11/12   max-w-maxContent flex-col items-center justify-between gap-8 text-rich-">
+      <div className="relative mx-auto my-10 flex w-11/12  max-w-maxContent flex-col items-center justify-between gap-8 text-rich-">
         {/* Become a Instructor Button */}
         <Link to={"/signup"}>
-          <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-100 drop-shadow-[0_1.5px_rgba(255,255,255,0.75)] transition-all duration-200 hover:scale-110 hover:drop-shadow-none">
+          <div className="group mx-auto mt-16 w-fit rounded-full bg-black p-1 font-bold text-white drop-shadow-[0_1.5px_rgba(255,255,255,0.75)] transition-all duration-200 hover:scale-110 hover:drop-shadow-none">
             <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
               <p>Become an Instructor</p>
               <FaArrowRight />
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js', // Ensures PostCSS configuration is used
+    postcss: './postcss.config.cjs', // Ensures PostCSS configuration is used
   },
   server: {
     port: 3000, // Set the port to 3000
